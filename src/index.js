9e090e3bcfa21f9543a42203507ed295aefc82de
Raynara Pimenta <raynarapimenta@gmail.com>
2022-06-15 16:44:41 -0300

    Finalização do estilo desktop, tablet e mobile

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import cipher from './cipher.js';
 
 const stringDeEntrada = document.getElementById("entrada");
-const numDeslocamento = document.getElementById("deslocamento");
+const numDeslocamento = document.getElementById("numero-deslocamento");
 const cifrar = document.getElementById("cifrar");
 const descifrar = document.getElementById("descifrar");
 const stringDeSaida = document.getElementById("saida");
-const showModal = document.getElementById("abrir-modal");
-const hideModal = document.getElementById("fechar-modal");
 
 cifrar.addEventListener("click", function () {
   stringDeSaida.value = cipher.encode(parseInt(numDeslocamento.value),stringDeEntrada.value);   
 }, false);
 
 descifrar.addEventListener("click", function () {
-  stringDeSaida.value = cipher.decode(parseInt(numDeslocamento.value), stringDeEntrada.value);
+  stringDeSaida.value = cipher.decode(parseInt(numDeslocamento.value), stringDeSaida.value);
 }, false);
+
+
+const showModal = document.getElementById("abrir-modal");
+const hideModal = document.getElementById("fechar-modal");
 
 function instruct () {
     let modal = document.querySelector('.modal')
