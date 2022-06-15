e360e0ee6f08b3f9710acd1b7496e267ae74752c
Raynara Pimenta <raynarapimenta@gmail.com>
2022-06-15 17:40:53 -0300

    Updates

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 import cipher from './cipher.js';
 
-const botao1 = document.getElementById("botao1")                        // botão para cifrar
-const botao2 = document.getElementById("botao2")                        // botão para decifrar
-const cartaEntrada = document.getElementById("digiteAqui");             // carta colocada para cifrar ou decifrar
-const numDeslocamento = document.getElementById("deslocamento");        // número do deslocamento
-const resultado = document.getElementById("textocifrado");              // campo com o resultado da carta de entrada + o deslocamento
+const stringDeEntrada = document.getElementById("entrada");
+const numDeslocamento = document.getElementById("numero-deslocamento");
+const cifrar = document.getElementById("cifrar");
+const descifrar = document.getElementById("descifrar");
+const stringDeSaida = document.getElementById("saida");
 
-botao1.addEventListener("click", function () {                          // quado clicar o botão1 (cifrar) vai executar a função click que gera o resultado baseado no: encode, depois deslocamento e por ultimo na mensagem
-  resultado.value = cipher.encode(parseInt(numDeslocamento.value),cartaEntrada.value);   
-}, false);                            
+cifrar.addEventListener("click", function () {
+  stringDeSaida.value = cipher.encode(parseInt(numDeslocamento.value),stringDeEntrada.value);   
+}, false);
 
-botao2.addEventListener("click", function () {                          // quado clicar o botão1 (decifrar) vai executar a função click que gera o resultado baseado no: deencode, depois deslocamento e por ultimo na mensagem
-  resultado.value = cipher.decode(parseInt(numDeslocamento.value), cartaEntrada.value);
+descifrar.addEventListener("click", function () {
+  stringDeSaida.value = cipher.decode(parseInt(numDeslocamento.value), stringDeSaida.value);
 }, false);
 
 
+const showModal = document.getElementById("abrir-modal");
+const hideModal = document.getElementById("fechar-modal");
 
-// entender melhor como funciona o addEventListene
+function instruct () {
+    let modal = document.querySelector('.modal')
+    modal.style.display = 'block';
+}
+showModal.addEventListener('click', instruct)
+
+function instructOff () {
+    let modal = document.querySelector('.modal')
+    modal.style.display = 'none';
+}
+hideModal.addEventListener('click', instructOff)
